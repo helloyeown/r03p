6977e25d419d08a4e80b3410f35e5b229b078888
hiYewonMoon1 <toquasimodox@gmail.com>
2023-10-12 17:59:07 +0900

    counter

--- src/components/counter/CountDisplay.js
+++ src/components/counter/CountDisplay.js
@@ -3,12 +3,13 @@ import { useSelector } from "react-redux";
 const CountDisplay = () => {
 
     // 스토어에서 받고 싶은 데이터 선별
-    // const obj = useSelector(state => state.counter)
+    const obj = useSelector(state => state.counter)
+    console.log(obj)
 
 
     return (  
         <div className="text-4xl">
-            count:
+            count: {obj.num}
         </div>
     );
 }
